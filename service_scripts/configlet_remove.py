338c3fd8f7986f36dd40938658053b6aecf43b12
RyanM-Arista <>
2022-01-27 22:31:32 -0500

    configlet remove script update

--- service_scripts/configlet_remove.py
+++ service_scripts/configlet_remove.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import urllib3, yaml
-from pprint import pprint
+#from pprint import pprint
 from cvprac.cvp_client import CvpClient
 urllib3.disable_warnings()
 clnt = CvpClient()
@@ -15,11 +15,11 @@ configletName = 'ATD-INFRA'
 container = clnt.api.get_container_by_name(container_name)
 configlet = clnt.api.get_configlet_by_name(configletName)
 result = clnt.api.remove_configlets_from_container("test", container, [configlet], True)
-pprint (result)
-print (result['data']['taskIds'])
+#pprint (result)
+#print (result['data']['taskIds'])
 task = result['data'].get('taskIds')
 for item in task:
-    print (item)
+    #print (item)
     itemExecute = clnt.api.execute_task(item)
-    print (itemExecute)
+    #print (itemExecute)
 #result2 = clnt.api.execute_task(result['data']['taskIds'])
